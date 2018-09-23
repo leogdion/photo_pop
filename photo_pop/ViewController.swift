715a3128d962bd4b2564b1eecebef51b55bd7d95
Leo Dion <leogdion@brightdigit.com>
2018-09-23 15:26:08 -0400

    Initial Commit

---
+++ photo_pop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  photo_pop
+//
+//  Created by Leo Dion on 9/23/18.
+//  Copyright © 2018 Bright Digit, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
