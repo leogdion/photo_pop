35a13e6e50a1ea51fe00f744c105818e7f1ff73a
Leo Dion <leogdion@brightdigit.com>
2018-09-23 20:09:05 -0400

    added initial code

--- photo_pop/ViewController.swift
+++ photo_pop/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  @IBOutlet var originalImageView: UIImageView!
+  @IBOutlet var poppedImageView: UIImageView!
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    let photoURL = Bundle.main.url(forResource: "PHOTO", withExtension: "HEIC")!
+    //let source = CGImageSourceCreateWithURL(photoURL as CFURL, nil)
+    let depthImage = CIImage(contentsOf: photoURL, options: [CIImageOption.auxiliaryDepth : true])!
+    
+    let disparityImage = depthImage.applyingFilter("CIDepthToDisparity")
+    
+    let image = UIImage(ciImage: disparityImage)
+    poppedImageView.image = image
   }
 
 
